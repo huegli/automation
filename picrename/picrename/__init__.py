105d3d21b63ccb57dab477da9bc59eb41d294d63
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-06-24 17:46:56 -0700

    Updates to command line argument processing

--- picrename/picrename/__init__.py
+++ picrename/picrename/__init__.py
@@ -13,30 +13,29 @@ def main():
 
     parser.add_argument('-a', '--alpha_index', 
             help = "Alphabet character to use for part of the new picture "
-                "name (e.g. --a 'A')",
-            action = 'store_true',
-            default = False)
+                "name (e.g. -a 'A')",
+            action = 'store',
+            default = 'A')
 
     parser.add_argument('-n', '--num_index',
             help = "Numerical index at which to start numbering "
                 "(e.g. -n '001')",
-            action = 'store_true',
-            default = False)
+            action = 'store',
+            default = '001')
 
     parser.add_argument('-v', '--verbose',
             help = "Print the new names of the picture files",
-            action = 'store_true',
-            default = False)
+            action = 'store_true')
+
     parser.add_argument('-d', '--debug_only',
             help = "Just print the new names without actually renaming " 
                 "the picture files",
-            action = 'store_true',
-            default = False)
+            action = 'store_true')
 
     parser.add_argument('dirname',
             help = "Directory name in which to rename the pictures")
 
     args = parser.parse_args()
 
-    renops.rename_all(".", 'A', '001')
+    renops.rename_all(args.dirname, args.alpha_index , args.num_index)
 
